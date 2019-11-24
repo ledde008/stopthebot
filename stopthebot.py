13716904330a92088843c9da8534070801e1f9c0
ledde008 <45053664+ledde008@users.noreply.github.com>
2019-11-24 16:03:11 -0500

    Added Decision Tree and Naive Bayes
    Not sure if correct

--- stopthebot.py
+++ stopthebot.py
@@ -10,6 +10,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.ensemble import GradientBoostingClassifier
 
+#Liam imports
+# Test of a naive bayes algorithm, the "fit" is the training
+from sklearn.naive_bayes import MultinomialNB #Is the dataset multinomial distributed? or do i use a different NaiveBayes
+from sklearn.tree import DecisionTreeClassifier #To implement decision tree model
+#End of Liam imports
+  
 # columns
 categorical_columns = ["Proto","SrcAddr","Sport","DstAddr","Dport","State"]
 numerical_columns =  ["Dur","TotPkts","SrcBytes","TotBytes"]
@@ -46,6 +52,10 @@ preprocessor = ColumnTransformer(
 clf_svm = Pipeline(steps=[('preprocessor', preprocessor), ('classifier', SVC(gamma="auto"))])
 clf_gbt = Pipeline(steps=[('preprocessor', preprocessor), ('classifier', GradientBoostingClassifier(max_depth=3))])
 
+#Liam Classifiers
+#clf_NB = MultinomialNB().fit(train_counts, train_tags)  #Need to actually find counts and tags... unless thats done above and i dont understand 
+clf_NB = Pipeline(steps=[('preprocessor', preprocessor), ('classifier', MultinomialNB())]) #Is this correct? i dont understand the 'Pipeline' part thing
+clf_DT = Pipeline(steps=[('preprocessor', preprocessor), ('classifier', DecisionTreeClassifier())]) #I believe this is how to do decision tree
 
 # Spliting data for training and testing
 X = dataframe.drop("Label", axis=1)
@@ -54,8 +64,17 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 clf_svm.fit(X_train, y_train)
 clf_gbt.fit(X_train, y_train)
 
+#Liam training
+clf_NB.fit(X_train, y_train)
+clf_DT.fit(X_train, y_train)
+
+
 # print the scores
 print("Testing the SVM Model")
 print("SVM Model score: %.3f" % clf_svm.score(X_test, y_test))
 print("Testing the GBT Model")
 print("GBT Model score: %.3f" % clf_gbt.score(X_test, y_test))
+print("Testing the NB Model")
+print("NB Model score: %.3f" % clf_NB.score(X_test, y_test))
+print("Testing Decision Tree Model")
+print("DecisionTree Model score: %.3f" % clf_DT.score(X_test, y_test))
